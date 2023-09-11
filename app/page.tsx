2ba30d704fd7365fc8519fbdd022d000dbd78892
sam <samwoodhams@proton.me>
2023-09-11 18:39:11 +0100

    Updated to use system preference for darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { useState } from 'react'
 
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
   return (
     <div className={darkMode ? "dark" : ""}>
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-800">
